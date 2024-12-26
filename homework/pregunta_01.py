fa3097b460f39ffbcd282d48f6217cd8645704e0
Mariana Hernandez <marianahernandez@MacBook-Pro-de-Mariana.local>
2024-12-26 10:53:21 -0500

    feat: Homework

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -2,6 +2,8 @@
 Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
+import pandas as pd
+
 # pylint: disable=import-outside-toplevel
 
 
@@ -15,6 +17,44 @@ def pregunta_01():
       espacios por guiones bajos.
     - Las palabras clave deben estar separadas por coma y con un solo
       espacio entre palabra y palabra.
-
-
     """
+
+    # Leer archivo
+    with open("files/input/clusters_report.txt", "r") as f:
+        lineas = f.readlines()
+        cluster = None
+        data = []
+        for linea in lineas[4:]:
+            linea = linea.strip()
+            parte = linea.split()
+            if linea:
+                if parte[0].isdigit():
+                    cluster = {
+                        "cluster": int(parte[0]),
+                        "cantidad_de_palabras_clave": int(parte[1]),
+                        "porcentaje_de_palabras_clave": float(
+                            parte[2].replace(",", ".").replace("%", "")
+                        ),
+                        "principales_palabras_clave": " ".join(parte[3:]).lstrip("%"),
+                    }
+                else:
+                    cluster["principales_palabras_clave"] += " " + " ".join(parte)
+
+            else:
+                data.append(cluster)
+        df = pd.DataFrame(data)
+
+        df["principales_palabras_clave"] = (
+            df["principales_palabras_clave"]
+            .str.replace(r"\s+", " ", regex=True)  # Normalizar espacios
+            .str.replace(r",\s*", ", ")  # Espacio después de comas
+            .str.strip(". ")
+        )
+
+        df.columns = [col.lower().replace(" ", "_") for col in df.columns]
+        print(df)
+
+    return df
+
+
+print(pregunta_01())
